c5ecd68140779f816df049d789e7f86c23866be0
Jacky Lo <jljcky@aim.com>
2014-04-25 22:09:43 -0400

    added BST and Node

---
+++ 16-BST/BST.java
@@ -0,0 +1,21 @@
+public class BST{
+    private Node root;
+
+    public BST(){
+	root = null;
+    }
+
+    public void insert(Node n){
+
+    }
+
+    public Node search(){
+	return null;
+    }
+
+    public Node search2(Node c, int x){
+	return null;
+    }
+
+}
+    
