828920316a1140c34044e5c02d786988deacd4c5
Jacky Lo <jljcky@aim.com>
2014-04-27 21:52:47 -0400

    edited bst

--- 16-BST/BST.java
+++ 16-BST/BST.java
@@ -6,15 +6,43 @@ public class BST{
     }
 
     public void insert(Node n){
-
+	if (root == null)
+	    root = n;
+	else{
+	    Node temp = root;
+	    while (temp != null){
+		if (n.getData() < temp.getData())
+		    temp = temp.getLeft();
+		else
+		    temp = temp.getRight();
+	    }
+	    temp = n;
+	}
     }
 
-    public Node search(){
-	return null;
+    public Node search(int x){
+	Node temp = root;
+	while (temp != null && temp.getData() != x){
+	    if (x < temp.getData())
+		temp = temp.getLeft();
+	    else
+		temp = temp.getRight();
+	}
+	return temp;
     }
 
     public Node search2(Node c, int x){
-	return null;
+        if (c == null)
+	    return null;
+	else if (c.getData() == x)
+	    return c;
+	else{
+	    if (x < c.getData())
+		search(c.getLeft(),x);
+	    else
+		search(c.getRight(),x);
+	}
+	
     }
 
 }
